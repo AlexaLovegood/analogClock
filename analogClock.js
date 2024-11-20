976058ab427852fb8c7f57457def1e0185b33990
AlexaLovegood <alexandra.ehrensperger5@gmail.com>
2024-11-20 16:24:22 +0100

    Comments

--- analogClock.js
+++ analogClock.js
@@ -15,24 +15,18 @@ function updateDate() {
   minute = now.getMinutes();
   second = now.getSeconds();
 }
-//checker update
-function checker(timeChecker, maxNum) {
-  if (timeChecker === maxNum) {
-    timeChecker = 0;
-  } else {
-    timeChecker++;
-  }
-}
 
-//second checker
-function secondHand() {
+// move the second-hand
+function moveSecondHand() {
   if (secondChecker === second) {
+    // set the movement
     let moveDeg = move60 * secondChecker;
-    //move secondHand
+    //change rotation prop
     document.documentElement.style.setProperty(
       "--rotation-second",
       `${moveDeg}deg`
     );
+    //increment the second checker
     if (secondChecker === 59) {
       secondChecker = 0;
     } else {
@@ -40,15 +34,17 @@ function secondHand() {
     }
   }
 }
-//minhand
-function minuteHand() {
+// move the minute hand
+function moveMinuteHand() {
   if (minuteChecker === minute) {
+    // set the movement
     let moveDeg = move60 * minuteChecker;
-    //move secondHand
+    //change rotation prop
     document.documentElement.style.setProperty(
       "--rotation-minute",
       `${moveDeg}deg`
     );
+    //increment the minute checker
     if (minuteChecker === 59) {
       minuteChecker = 0;
     } else {
@@ -57,16 +53,18 @@ function minuteHand() {
   }
 }
 
-//hourhand
-function hourHand() {
+// move the hour hand
+function moveHourHand() {
   if (hourChecker === hour) {
+    // set the movement
     let moveDeg = move12 * hourChecker;
-    //move secondHand
+    //change rotation prop
     document.documentElement.style.setProperty(
       "--rotation-hour",
       `${moveDeg}deg`
     );
-    if (hourChecker === 59) {
+    //increment the hour checker
+    if (hourChecker === 23) {
       hourChecker = 0;
     } else {
       hourChecker++;
@@ -74,7 +72,8 @@ function hourHand() {
   }
 }
 
+//repeat the function every 100 milliseconds
 setInterval(updateDate, 100);
-setInterval(secondHand, 100);
-setInterval(minuteHand, 100);
-setInterval(hourHand, 100);
+setInterval(moveSecondHand, 100);
+setInterval(moveMinuteHand, 100);
+setInterval(moveHourHand, 100);
