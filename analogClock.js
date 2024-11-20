852786bed6e1d5cdcd965dddaed687400efb96db
AlexaLovegood <alexandra.ehrensperger5@gmail.com>
2024-11-20 14:28:22 +0100

    Second-hand works!

--- analogClock.js
+++ analogClock.js
@@ -4,11 +4,25 @@ let hour = now.getHours();
 let minute = now.getMinutes();
 let second = now.getSeconds();
 let secondChecker = second;
-
+let move60 = 360 / 60;
+//update date
+function updateDate() {
+  now = new Date();
+  hour = now.getHours();
+  minute = now.getMinutes();
+  second = now.getSeconds();
+}
+//document.documentElement.style.setProperty("--rotation-second", `${move60}deg`);
+secondHand(second, secondChecker, move60);
 //second checker
-function secondHand(second) {
+function secondHand() {
   if (secondChecker === second) {
+    let moveDeg = move60 * secondChecker;
     //move secondHand
+    document.documentElement.style.setProperty(
+      "--rotation-second",
+      `${moveDeg}deg`
+    );
     if (secondChecker === 59) {
       secondChecker = 0;
     } else {
@@ -16,3 +30,6 @@ function secondHand(second) {
     }
   }
 }
+
+setInterval(updateDate, 100);
+setInterval(secondHand, 100);
