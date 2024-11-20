3190ff65c4fed6e527c32b20bfcdf84dc8c976c0
AlexaLovegood <alexandra.ehrensperger5@gmail.com>
2024-11-20 09:26:56 +0100

    Outline of second-hand function

--- analogClock.js
+++ analogClock.js
@@ -3,3 +3,16 @@ let now = new Date();
 let hour = now.getHours();
 let minute = now.getMinutes();
 let second = now.getSeconds();
+let secondChecker = second;
+
+//second checker
+function secondHand(second) {
+  if (secondChecker === second) {
+    //move secondHand
+    if (secondChecker === 59) {
+      secondChecker = 0;
+    } else {
+      secondChecker++;
+    }
+  }
+}
